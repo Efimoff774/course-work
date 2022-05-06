0f5ba8372c9097fd94e466628738dccd58be23cf
daniil <daniilefimoff774@gmail.com>
2022-05-07 00:44:11 +0300

    project has finished

--- client/src/components/modals/CreateDevice.jsx
+++ client/src/components/modals/CreateDevice.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/esm/Button'
 import Form from 'react-bootstrap/Form'
@@ -6,16 +6,49 @@ import { Context } from '../..'
 import Dropdown from 'react-bootstrap/Dropdown'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
-const CreateDevice = ({ show, onHide }) => {
+import { createDevice, fetchBrands, fetchDevices, fetchTypes } from '../../http/deviceAPI'
+import { observer } from 'mobx-react-lite'
+const CreateDevice = observer(({ show, onHide }) => {
 
     const { device } = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
+    const [brand, setBrand] = useState(null)
+    const [type, setType] = useState(null)
     const [info, setInfo] = useState([])
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+        fetchBrands().then(data => device.setBrands(data))
+        fetchDevices().then(data => device.setDevices(data.rows))
+
+    }, [])
 
     const addInfo = () => {
         setInfo([...info, { title: '', description: '', number: Date.now() }])
     }
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
+    }
+
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? { ...i, [key]: value } : i))
+    }
+
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    const addDevice = () => {
+        const formData = new FormData()
+        formData.append('name', name)
+        formData.append('price', `${price}`)
+        formData.append('img', file)
+        formData.append('brandId', device.selectedBrand.id)
+        formData.append('typeId', device.selectedType.id)
+        formData.append('info', JSON.stringify(info))
+        createDevice(formData).then(data => onHide())
     }
 
     return (
@@ -33,26 +66,30 @@ const CreateDevice = ({ show, onHide }) => {
             <Modal.Body>
                 <Form>
                     <Dropdown className='mt-3'>
-                        <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedType.name || 'Выберите тип'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.types.map(type =>
-                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                <Dropdown.Item onClick={() => device.setSelectedType(type)} key={type.id}>{type.name}</Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown >
                     <Dropdown className='mt-3'>
-                        <Dropdown.Toggle>Выберите бренд</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedBrand.name || 'Выберите бренд'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.brands.map(brand =>
-                                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                                <Dropdown.Item onClick={() => device.setSelectedBrand(brand)} key={brand.id}>{brand.name}</Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                         className='mt-3'
                         placeholder='Введите название айтема'
                     />
                     <Form.Control
+                        value={price}
+                        onChange={e => setPrice(Number(e.target.value))}
                         className='mt-3'
                         placeholder='Введите стоимость айтема'
                         type='number'
@@ -60,6 +97,7 @@ const CreateDevice = ({ show, onHide }) => {
                     <Form.Control
                         className='mt-3'
                         type='file'
+                        onChange={selectFile}
                     />
                 </Form>
                 <hr />
@@ -68,11 +106,15 @@ const CreateDevice = ({ show, onHide }) => {
                     <Row className='mt-3' key={i.number}>
                         <Col md={4} className={'mt-2'}>
                             <Form.Control
+                                value={i.title}
+                                onChange={(e) => changeInfo('title', e.target.value, i.number)}
                                 placeholder='Введите название свойства'
                             />
                         </Col>
                         <Col md={4} className={'mt-2'}>
                             <Form.Control
+                                value={i.description}
+                                onChange={(e) => changeInfo('description', e.target.value, i.number)}
                                 placeholder='Введите описание свойства'
                             />
                         </Col>
@@ -85,11 +127,11 @@ const CreateDevice = ({ show, onHide }) => {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
-                <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+                <Button variant='outline-success' onClick={addDevice}>Добавить</Button>
 
             </Modal.Footer>
         </Modal>
     )
-}
+})
 
 export default CreateDevice
--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row'
 import { Context } from '..'
 import BrandBar from '../components/BrandBar'
 import DeviceList from '../components/DeviceList'
+import Pages from '../components/Pages'
 import TypeBar from '../components/TypeBar'
 import { fetchBrands, fetchDevices, fetchTypes } from '../http/deviceAPI'
 
@@ -15,23 +16,33 @@ const Shop = observer(() => {
     useEffect(() => {
         fetchTypes().then(data => device.setTypes(data))
         fetchBrands().then(data => device.setBrands(data))
-        fetchDevices().then(data => device.setDevices(data.rows))
-
+        fetchDevices(null, null, 1, 2).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalCount(data.count)
+        })
     }, [])
+
+    useEffect(() => {
+        fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalCount(data.count)
+        })
+    }, [device.page, device.selectedType, device.selectedBrand,])
 
     return (
         <Container>
-            <Row className='mt-3'>
+            <Row className="mt-2">
                 <Col md={3}>
                     <TypeBar />
                 </Col>
                 <Col md={9}>
                     <BrandBar />
                     <DeviceList />
+                    <Pages />
                 </Col>
             </Row>
         </Container>
-    )
-})
+    );
+});
 
 export default Shop
